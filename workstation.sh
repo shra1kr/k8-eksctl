5687b26326ba4872228001661fc255e9fa2b2dd8
shravan <shravankumar6262@gmail.com>
2024-07-14 15:29:28 +0530

    committed by shravan

--- workstation.sh
+++ workstation.sh
@@ -37,10 +37,10 @@ usermod -aG docker ec2-user
 VALIDATE $? "Docker installation"
 
 # eksctl
-curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
-mv /tmp/eksctl /usr/local/bin
-eksctl version
-VALIDATE $? "eksctl installation"
+# curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
+# mv /tmp/eksctl /usr/local/bin
+# eksctl version
+# VALIDATE $? "eksctl installation"
 
 
 # kubectl
